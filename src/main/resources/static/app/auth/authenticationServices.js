c68b6e97fa3f676a2de6f02991ff809f31f5b9b2
Artur Paszkiewicz <paszkiewicz.artur@gmail.com>
2017-11-25 20:54:52 +0100

    4

--- src/main/resources/static/app/auth/authenticationServices.js
+++ src/main/resources/static/app/auth/authenticationServices.js
@@ -2,7 +2,7 @@ angular.module('app')
 .constant('LOGIN_ENDPOINT', '/login')
 .service('AuthenticationService', function($http, LOGIN_ENDPOINT) {
 	this.authenticate = function(credentials, successCallback) {
-		var authHeader = {Authorization: 'Basic ' + btoa(credentials.username+':'+credentials.password)};
+		var authHeader = {Authorization: 'Basic ' + btoa(credentials.email+':'+credentials.password)};
 		var config = {headers: authHeader};
 		$http
 		.post(LOGIN_ENDPOINT, {}, config)
