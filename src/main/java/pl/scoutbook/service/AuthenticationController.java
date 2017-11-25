3a892be9eea8812208fffcd43de6118e8bda8f26
Artur Paszkiewicz <paszkiewicz.artur@gmail.com>
2017-11-25 20:47:32 +0100

    3

---
+++ src/main/java/pl/scoutbook/service/AuthenticationController.java
@@ -0,0 +1,16 @@
+package pl.scoutbook.service;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AuthenticationController {
+    @PostMapping("/login")
+    @ResponseBody
+    public Principal login(Principal user) {
+        return user;
+    }
+}
---
+++ src/main/resources/static/app/failedLogin/failedLoginController.js
@@ -0,0 +1,8 @@
+﻿angular.module('scoutbookApp')
+.controller('failedLoginController', function($rootScope, $location, $state, $scope, AuthenticationService) {
+	$rootScope.credentials = {};
+	$scope.login = function() {
+		console.log("inside login");
+		AuthenticationService.authenticate($rootScope.credentials);
+	}
+});
